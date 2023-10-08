fefaebfc02ab4da060c8681bc0a6dffaa5902a5f
Prasuna Dash <110610976+dprasuna@users.noreply.github.com>
2023-10-09 00:22:37 +0530

    First commit

---
+++ 02_basics/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
